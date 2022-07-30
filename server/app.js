7072dd8123a30d7104319a2325415e8431cb8f2c
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-07-30 17:03:21 -0400

    Frontend Installs Complete

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(logger('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => res.json({ message: 'Server Running!' }))
+app.listen(PORT, () =>
+  console.log(`Serving up fresh reads on port: ${PORT} 📚`)
+)
