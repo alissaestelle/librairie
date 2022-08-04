1e59dc725adc7d57477da69a5b8211add401bfa9
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-08-04 16:46:43 -0400

    Notes Added to <Pagination>

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,29 +1,34 @@
-import { useState } from 'react'
-
 const Pagination = ({ currentPage, pageIndex, pageRange, setCurrent }) => {
-  const [pageClass, setClass] = useState(false)
   let thesePages
 
   const nextPage = () => {
     setCurrent((page) => parseInt(page) + 1)
+    // Takes the previous page value and increments it by 1
   }
 
   const prevPage = () => {
     setCurrent((page) => parseInt(page) - 1)
+    // Takes the previous page value and decrements it by 1
   }
 
   const changePage = (e) => {
     let thisPage = parseInt(e.target.value)
     setCurrent(thisPage)
+    // Sets [currentPage] to any page chosen in the React Map
   }
 
+  // [getFivePages] updates page numbers with the next set of pages in the pagination
   const getFivePages = () => {
     let start = Math.floor(currentPage / pageRange) * pageRange
+    // Translation: start = Math.floor(currentPage / 5) * 5
     thesePages = pageIndex.map(() => (start = start + 1))
+    // Translation: Map the values for each idx of [pageIndex], starting with x ([start]) and incrementing by one each time ([pageIndex].length = 5)
     return thesePages
   }
 
   getFivePages()
+
+  // [thesePages] maps the page numbers to each button in the pagination:
 
   return (
     <div className="pagination">
@@ -39,7 +44,8 @@ const Pagination = ({ currentPage, pageIndex, pageRange, setCurrent }) => {
               key={idx}
               value={page}
               onClick={changePage}
-              className={currentPage === parseInt(page) ? 'active' : null}
+              // Self To-Do: Finish Fx to Show Current Page Button as 'Active'
+              // className={currentPage === parseInt(page) ? 'active' : null}
               disabled={currentPage >= thesePages.length}
             >
               {page}
